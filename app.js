673ee133c0e1bfddbe7330ebfe43aa7fbece1976
harshkhatri04 <harshkhatri04@gmail.com>
2017-08-28 20:11:35 +0530

    express

---
+++ app.js
@@ -0,0 +1,54 @@
+ //importing modules
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const route = require('./routes/route');
+
+//connect to mongo db
+mongoose.connect('mongodb://localhost:27017/contactlist');
+
+//on connect
+mongoose.connection.on('connected',()=>{
+	console.log('connected to db')
+})
+
+mongoose.connection.on('error',(err)=>{
+	if(err)
+	{
+		console.log('error in connecting db:'+err);
+	}
+})
+
+//port 
+const port = 8090;
+
+//adding middleware -cors
+app.use(cors());
+
+//body - parser
+app.use(bodyparser.json());
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+//routes
+app.use('/api',route);
+
+//testing server
+app.get('/',(req,res)=>{
+	 res.send('testing server')
+})
+
+app.listen(port,()=> {
+  console.log('app listening on port 3000!')
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
